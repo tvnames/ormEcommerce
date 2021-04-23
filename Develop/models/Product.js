cf8c71bdb503b46458047e281d73cfaf269b6e31
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-23 15:32:57 -0700

    Added product.js model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { primaryKeyAttribute } = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+ 
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+   validate: {
+     isNumeric: true
+   }
+
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: Category.category_id
+  },
   },
   {
     sequelize,
