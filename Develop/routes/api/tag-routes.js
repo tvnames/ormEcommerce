e8c54633f382e8a574a5f8d38356ee54a4c2cd90
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-24 12:54:00 -0700

    Created paths for insomnia requests

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,24 +5,69 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);  
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const tagData = await Tag.findByPk({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);  
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+ 
+    res.status(200).json(tagData);  
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+ 
+    res.status(200).json(tag);  
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+ 
+    res.status(200).json(tagData);  
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
